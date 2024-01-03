3e6947380aa3ed37f92cad8f3777e2ae7e9628c3
vandana7566 <vandanaswarupa1174@gmail.com>
2024-01-03 22:48:40 +0000

    added stash

--- hello.java
+++ hello.java
@@ -2,6 +2,10 @@ class Hello
 {
 Public static void main(String args[])
 {
+<<<<<<< Updated upstream
 System.out.println(“hello to child”);
 
+=======
+System.out.println(“hello from child”);
+>>>>>>> Stashed changes
 }
